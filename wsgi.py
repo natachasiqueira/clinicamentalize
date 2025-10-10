ad590c86797000bb0a70e5259a820e929037f120
Natacha <natacha.seragiotto@gmail.com>
2025-10-10 19:47:57 -0300

    Melhorias no sistema.

--- wsgi.py
+++ wsgi.py
@@ -81,4 +81,5 @@ if os.getenv('FLASK_CONFIG') == 'production':
     init_database_if_needed()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
